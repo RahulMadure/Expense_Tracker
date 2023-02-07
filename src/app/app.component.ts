8789c9a1e2c4aa8de192138c317b436788fb5994
Rahul Madure <rahulmadure99@gmail.com>
2023-02-08 02:23:32 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Expense_Tracker';
+  title = 'Expense Tracker';
+  expenses: {item: string, amount: number}[] = [];
+  totalExpense = 0;
+  newExpense = {item: '', amount: 0};
+
+  addExpense() {
+    if (this.newExpense.item && this.newExpense.amount) {
+      this.expenses.push({ ...this.newExpense });
+      this.totalExpense += this.newExpense.amount;
+      this.newExpense = {item: '', amount:0};
+    }
+  }
+
+  removeExpense(index: number) {
+    this.totalExpense -= this.expenses[index].amount;
+    this.expenses.splice(index, 1);
+  }
 }
